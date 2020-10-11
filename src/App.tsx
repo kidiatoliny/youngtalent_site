24f1f68caf97c2d1b62cedd36611e882dc3cf57b
kid <kidiatoliny@gmail.com>
2020-10-11 22:33:14 -0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './styles/GlobalStyles'
+import Section from './components/section'
+import RigthSideInformation from './components/RightSideInformation'
+import {data, socialMedia, departaments,partners} from './data'
+import Logo from './components/logo';
+import Icon from './components/Icon';
+import SocialIcon from './components/SocialIcon';
+import { DepartamentContainer } from './components/section/styles';
+import Departaments from './components/Departaments';
 
+import Card from './components/Card';
+import PartnersCard from './components/PartnersCard';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Section
+    variant="main"
+    title={data[0].title}
+    description={data[0].description}
+    />
+        <Section variant="departaments">
+     <DepartamentContainer>
+
+       <h1>Nossos Departamentos</h1>
+       <Departaments>
+        {
+          departaments.map(departament=>
+            <Card
+            name={departament.name}
+            url={departament.url}
+            img={departament.img}
+            mentor={departament.mentor}
+            />
+          )
+        }
+       </Departaments>
+
+     </DepartamentContainer>
+     </Section>
+      <Section
+    variant="partners">
+
+
+<DepartamentContainer color="white">
+
+<h1>Nossos Parceiros</h1>
+<Departaments>
+     <PartnersCard partners={partners}/>
+</Departaments>
+</DepartamentContainer>
+    </Section>
+
+      <Section
+    variant="tech"
+    title={data[3].title}
+    description={data[3].description}
+    />
+      <Section
+    variant="social"
+    title={data[4].title}
+    description={data[4].description}
+    />
+    <RigthSideInformation>
+      <Logo/>
+
+    <SocialIcon>
+    {
+        socialMedia.map(social=>(
+          <Icon icon={social.icon} url={social.url} name={social.name} size={48}/>
+        ))
+      }
+    </SocialIcon>
+
+    </RigthSideInformation>
+    <GlobalStyle/>
+    </>
   );
 }
 
